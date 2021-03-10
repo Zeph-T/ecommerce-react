42a4166659871e10c45c532104b9137565700b21
Thari Zephaniah <paul.zeph@gmail.com>
2021-03-10 13:48:02 +0530

    Addeed various componenets

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,27 @@
+import React,{Component} from 'react';
+import sections from "./directory.data";
+import MenuItem from "../menu-items/menu-item.component";
+import "./directory.styles.scss";
+
+class Directory extends Component {
+ constructor(){
+     super();
+     this.state={
+         sections:sections
+     }
+    }
+    render(){
+        return(
+            <div className="directory-menu">
+                {
+                    this.state.sections.map(({title,imageUrl,id,size}) => (
+                        <MenuItem key={id} title={title} image={imageUrl} size={size}/>
+                    ))
+                }
+            </div>
+        )
+    }   
+}
+
+
+export default Directory;
