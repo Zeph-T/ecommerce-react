7c10f58d95f813af9b253e0b053ebf46679585c2
Thari Zephaniah <paul.zeph@gmail.com>
2021-03-10 17:58:41 +0530

    added routes wiith React-Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from "react-router-dom";
 import Homepage from './pages/homepage/homepage.component';
+
+const Hatspage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+
+);
+
+
 function App() {
   return (
     <div>
-    <Homepage></Homepage>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={Hatspage} />
+      </Switch>
+
     </div>
   );
 }
