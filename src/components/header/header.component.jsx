c4143f40b62717d1ab784b09d322f9bbdacdc868
Thari Zephaniah <paul.zeph@gmail.com>
2021-03-12 17:00:56 +0530

    updated components which provide the functioanilty to add items to cart and update the quantity property if same item is selected more than once

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {connect} from 'react-redux';
-import { auth } from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
 
-import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-import CartIcon from "../cart-icon/cart-icon.component"
+
 import './header.styles.scss';
 
-const Header = ({ currentUser ,hidden }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -22,7 +24,6 @@ const Header = ({ currentUser ,hidden }) => (
       </Link>
       {currentUser ? (
         <div className='option' onClick={() => auth.signOut()}>
-          
           SIGN OUT
         </div>
       ) : (
@@ -32,17 +33,13 @@ const Header = ({ currentUser ,hidden }) => (
       )}
       <CartIcon />
     </div>
-    {
-      hidden ? null:
-      <CartDropdown />      
-    }
+    {hidden ? null : <CartDropdown />}
   </div>
 );
 
-const mapStateToProps = ({user:{currentUser},cart:{hidden}}) => ({
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
   currentUser,
   hidden
-
-})
+});
 
 export default connect(mapStateToProps)(Header);
