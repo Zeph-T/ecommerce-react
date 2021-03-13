4d6df886235eb1ab68bfc69c7f8986097e937daf
Thari Zephaniah <paul.zeph@gmail.com>
2021-03-13 12:33:04 +0530

    checkout-page implemented

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,9 +5,10 @@ import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-
+import {createStructuredSelector} from 'reselect';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
+import {selectCurrentUser} from "../../redux/user/user.selector";
 import './header.styles.scss';
 
 const Header = ({ currentUser, hidden }) => (
@@ -37,9 +38,9 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-  currentUser,
-  hidden
-});
+const mapStateToProps = createStructuredSelector({
+  currentUser:selectCurrentUser,
+  hidden:selectCartHidden
+})
 
 export default connect(mapStateToProps)(Header);
